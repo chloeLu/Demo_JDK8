3322c42a68e44f27f88895ee6de5d7adbeca6cf3
chloeLu <chloe.lxd@gmail.com>
2014-12-04 00:16:22 +0800

    InterfaceDemo

--- src/demo/InterfaceDemo.java
+++ src/demo/InterfaceDemo.java
@@ -2,17 +2,38 @@ package demo;
 
 
 public class InterfaceDemo {
+	public static void main (String[] args) {
+	}
 }
 
 @FunctionalInterface
 interface DemoInterface {
 	public void method();
 	default void defaultMethod() {}
-	static void staticMethod() {}
+	public static void staticMethod() {}
 }
 
 interface Plant {
-	default boolean isGrowing() {
+	default String name() {
+		return "plant";
+	}
+	public static boolean growing() {
 		return true;
+	}
+}
+
+interface Fruit {
+	default String name() {
+		return "fruit";
+	}
+	public static boolean growing() {
+		return false;
+	}
+}
+
+class Apple implements Plant, Fruit {
+	@Override
+	public String name() {
+		return Fruit.super.name();
 	}
 }
