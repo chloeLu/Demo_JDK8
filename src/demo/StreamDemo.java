af05e159a7953a24e5420e861beec6174935a142
chloeLu <chloe.lxd@gmail.com>
2014-12-05 00:00:25 +0800

    stream

--- src/demo/StreamDemo.java
+++ src/demo/StreamDemo.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamDemo {
 
@@ -16,53 +17,58 @@ public class StreamDemo {
 	}
 
 	public List<Person> createPersonList() {
-		return Arrays.asList(
-				new Person("Alice", 31), 
-				new Person("Bill", 39),
-				new Person("Tom", 10), 
-				new Person(null, 40),
-				new Person("George", 70),
-				new Person("Sark", 20));
+		return Arrays.asList(new Person("Alice", 31), new Person("Bill", 39),
+				new Person("Tom", 10), new Person(null, 40), new Person(
+						"George", 70), new Person("Sark", 20));
 	}
-	
+
 	/**
 	 * Goal: collects all names from pList
 	 */
 	public void demoSimple(List<Person> pList) {
-		List<String> names = pList.stream().map(p->p.getName()).collect(Collectors.toList());
+		List<String> names = pList.stream().map(p -> p.getName())
+				.collect(Collectors.toList());
 		printList(names);
 	}
-	
+
 	/**
 	 * Goal: collects all names from pList
 	 */
 	public void demoSimple_jdk6(List<Person> pList) {
 		List<String> names = new ArrayList<String>();
-		for (Person p: pList) {
+		for (Person p : pList) {
 			names.add(p.getName());
 		}
 		printList(names);
 	}
-	
+
 	/**
-	 * Goal: return a list of names:
-	 * 1. whose age is greater than 20, and
-	 * 2. sort in ascending order of age, and
+	 * Goal: print a list of names: 
+	 * 1. whose age is greater than 20, and 
+	 * 2. sort in ascending order of age
 	 */
-	public void demoWithMoreOperations (List<Person> pList) {
-		List<String> names =pList.parallelStream().filter(p -> p.getAge() > 20)
-									  .sorted(new Comparator<Person>() {
-										  public int compare(Person o1, Person o2) {
-											  return o1.getAge()-o2.getAge();
-										  }})
-									  .map(p->p.getName())
-									  .collect(Collectors.toList());
+	public void demoWithMoreOperations(List<Person> pList) {
+		List<String> names = pList.parallelStream()
+				.filter(p -> p.getAge() > 20).sorted(new Comparator<Person>() {
+					public int compare(Person o1, Person o2) {
+						return o1.getAge() - o2.getAge();
+					}
+				}).map(p -> p.getName()).collect(Collectors.toList());
 		printList(names);
 	}
-	
+
+	/**
+	 * Never call this
+	 */
+	public void demoInfiniteStream() {
+		Stream<Integer> infiniteStream = Stream.iterate(0, n -> n + 10);
+		Stream<Integer> fixedSizeStream = infiniteStream.limit(10);
+		fixedSizeStream.forEach(n->System.out.println(n));
+	}
+
 	// Helper
-	public void printList(List<String> list){
-		for (String s: list) {
+	public void printList(List<String> list) {
+		for (String s : list) {
 			System.out.println(s);
 		}
 	}
